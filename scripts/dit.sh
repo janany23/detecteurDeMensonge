c1fa52efc4426068c36dbd72fc497231fdf0ceaa
karineould <karine.ouldbraham@gmail.com>
2017-12-17 16:22:13 +0000

    fix path

--- scripts/dit.sh
+++ scripts/dit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo $1
-pico2wave -l fr-FR -w ../temp/test.wav "$1"
-aplay -q ../temp/test.wav
-rm ../temp/test.wav
+pico2wave -l fr-FR -w ./temp/test.wav "$1"
+aplay -q ./temp/test.wav
+rm ./temp/test.wav
