1fefd3520209da6f703d04d5a07ad5bdbfc74fe0
Karine Ouldbraham <k.ouldbraham@weezevent.com>
2017-12-21 10:30:37 +0100

    call recording script

--- routes/test.js
+++ routes/test.js
@@ -55,12 +55,25 @@ router.get('/playQuestion', function(req, res, next) {
                 var responseArdui = JSON.parse(body);
                 console.log(responseArdui[0].resultat);
 
+                //execute scipt to listen to the answer
+                exec('sh ./scripts/recording.sh "'+ id + '"', function (error, stdout, stderr)
+                    {
+                        console.log(stdout);
+                        console.log(stderr);
+                        if (error !== null) {
+                            console.log('exec error:' + error);
+                        }
+                    }
+                );
+                
                 //socket server emit the date from ardui
                 io.emit('data', {resultat: responseArdui[0].resultat, question: id});
             } else {
                 console.log('error try to get http://172.20.10.3:8 : ' + error.code);
             }
         });
+
+
     });
 
     res.redirect('/');
@@ -69,16 +82,7 @@ router.get('/playQuestion', function(req, res, next) {
 router.get('/playReponse', function(req, res, next) {
     var id = req.query.questionId;
 
-    //execute scipt to listen to the answer
-    exec('sh ./scripts/recording.sh "'+ question + '"', function (error, stdout, stderr)
-        {
-            console.log(stdout);
-            console.log(stderr);
-            if (error !== null) {
-                console.log('exec error:' + error);
-            }
-        }
-    );
+
 
 });
 
